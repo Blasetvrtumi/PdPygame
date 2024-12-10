e19b13cb7d7829dd65918fa65187bad39d81769b
SaraTuset <s.tuset.2020@alumnos.urjc.es>
2024-12-10 20:28:40 +0100

    Con botón de atras

--- our_cozy_games/menu_maker.py
+++ our_cozy_games/menu_maker.py
@@ -5,19 +5,29 @@ from pygame_menu import themes
 import main
 
 update_loading = pygame.USEREVENT + 0
+atras = False
 
 def lanzar_carga():
     pygame.time.set_timer(update_loading, 30)
 
+def lanzar_main():
+    global atras
+    atras = True
+    pygame.time.set_timer(update_loading, 30)
+
+
 def crear_carga(text = None):    
+    atras = False
     loading = pygame_menu.Menu('Loading the Game...', 600, 400, theme=themes.THEME_DARK)
     loading.add.progress_bar("Progress", progressbar_id="1", default=0, width=200)
     if text:
         loading.add.label(text)
         loading.add.button("Entendido", lanzar_carga)
+    loading.add.button("Volver al rpg", lanzar_main)
     return loading
 
 def pagina_carga(text = None, programa = None):   
+    global atras
     pygame.init()
     surface = pygame.display.set_mode((600, 400))
     arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size=(10, 15))
@@ -33,10 +43,11 @@ def pagina_carga(text = None, programa = None):
                 progress.set_value(progress.get_value() + 1)
                 if progress.get_value() == 100:
                     pygame.time.set_timer(update_loading, 0)
-                    if not programa:
-                        main.run()  # Lanza rpg
-                    else:
+                    print(atras)
+                    if programa and not atras:
                         programa()
+                    else:
+                        main.run()  # Lanza rpg
             if event.type == pygame.QUIT:
                 exit()
 
